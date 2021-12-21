ea57fc2732bb4a0f1a2ae30e3978619da567b541
toast <58362380+traumatism@users.noreply.github.com>
2021-12-21 01:07:50 +0100

    fixed typing

--- toastcord/widgets/header.py
+++ toastcord/widgets/header.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from rich.console import RenderableType
 from rich.table import Table
 
-from textual import events
 from textual.widget import Widget
 
 from toastcord import client, version
@@ -13,21 +12,22 @@ from toastcord.utils.panel import get_panel
 class Header(Widget):
 
     def render(self) -> RenderableType:
-        header_table = Table.grid(padding=(0, 1), expand=True)
-        header_table.style = self.style
-        header_table.add_column(justify="left", ratio=0, width=20)
-        header_table.add_column("title", justify="center", ratio=20)
-        header_table.add_column("clock", justify="right", width=20)
 
-        header_table.add_row(
+        table = Table.grid(padding=(0, 1), expand=True)
+
+        table.add_column(justify="left", ratio=0, width=20)
+        table.add_column("title", justify="center", ratio=20)
+        table.add_column("clock", justify="right", width=20)
+
+        table.add_row(
             str(client.user), f"[italic]ToastCord v{version}[/italic]",
             datetime.now().time().strftime("%X")
         )
 
         panel = get_panel()
-        panel.renderable = header_table
+        panel.renderable = table
 
         return panel
 
-    async def on_mount(self, event: events.Mount) -> None:
+    async def on_mount(self, event) -> None:
         self.set_interval(1.0, callback=self.refresh)
