ce249c955afd563c6880c0b06e2466d2575a8ed0
toast <58362380+traumatism@users.noreply.github.com>
2021-12-21 14:10:28 +0100

    README update

---
+++ toastcord/api/types/__init__.py
@@ -0,0 +1,11 @@
+from .channels import (
+    MessageChannel, GuildChannel, Channel
+)
+
+from .guild import Guild
+
+from .message import (
+    Message, ToastBotMessage
+)
+
+from .user import User
