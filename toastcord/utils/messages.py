2e0c664fabfba69796e1b892443043f5a58e6ea5
toast <cpastoast@protonmail.com>
2021-12-23 22:59:49 +0100

    Update

---
+++ toastcord/utils/messages.py
@@ -0,0 +1,45 @@
+import rich.repr
+
+from typing import Union
+
+from textual.message import Message, MessageTarget
+
+from toastcord.api.types.guild import Guild
+from toastcord.api.types.channels import Channel, GuildChannel, MessageChannel
+
+
+@rich.repr.auto
+class Click(Message, bubble=True):
+    """ Handle click """
+
+    def __init__(
+        self, sender: MessageTarget,
+        target: Union[Guild, Channel, GuildChannel, MessageChannel]
+    ) -> None:
+        self.target = target
+        super().__init__(sender)
+
+
+@rich.repr.auto
+class Key(Message, bubble=True):
+    """ Handle key press """
+
+    def __init__(self, sender: MessageTarget, target: str) -> None:
+        self.target = target
+        super().__init__(sender)
+
+
+@rich.repr.auto
+class ChannelChanged(Message, bubble=True):
+    """ Handle channel changing """
+
+    def __init__(self, sender: MessageTarget) -> None:
+        super().__init__(sender)
+
+
+@rich.repr.auto
+class MessageSent(Message, bubble=True):
+    """ Handle message sending """
+
+    def __init__(self, sender: MessageTarget) -> None:
+        super().__init__(sender)
